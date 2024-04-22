f9b1439cfc1cce2a01ba76094d0a476388ad2880
RichardENeumann <RichardNeumann@googlemail.com>
2024-04-22 14:43:56 +0200

    Implement basic check for username availability. Debounce remains to be done

--- src/index.js
+++ src/index.js
@@ -18,3 +18,20 @@ username.addEventListener("input", () => {
     username.setCustomValidity("");
   }
 });
+
+username.addEventListener("input", async () => {
+  const checkUserURL = "./logic/checkuser.php";
+  const checkUserOptions = new URLSearchParams({
+    username: username.value,
+  });
+
+  const result = await fetch(`${checkUserURL}?${checkUserOptions}`);
+  const responseBody = await result.text();
+  const usernameValid = JSON.parse(responseBody).valid;
+
+  if (usernameValid) {
+    console.log("yes");
+  } else {
+    console.log("no");
+  }
+});
