ec65a49b02f0df01522e795e4df935c65433d183
RichardENeumann <RichardNeumann@googlemail.com>
2024-04-24 14:37:15 +0200

    Implement congruency check on password fields

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Check if the entered email address is valid and give custom hints
 const email = document.getElementById("email");
 const username = document.getElementById("username");
 
@@ -9,17 +10,8 @@ email.addEventListener("input", () => {
   }
 });
 
-username.addEventListener("input", () => {
-  if (username.validity.tooShort) {
-    username.setCustomValidity("The username needs to be at least 8 characters long.");
-  } if (username.validity.tooLong) {
-    username.setCustomValidity("The username can not exceed 20 characters.");
-  } else {
-    username.setCustomValidity("");
-  }
-});
-
-function debounce(func, delay = 250) {
+// Check if the provided username is available and within constraints
+function debounce(func, delay = 500) {
   let timerId;
 
   return (...args) => {
@@ -40,13 +32,46 @@ async function checkUsernameAvailable() {
   const responseBody = await result.text();
   const usernameValid = JSON.parse(responseBody).valid;
 
-  if (usernameValid) {
-    console.log("yes");
-  } else {
-    console.log("no");
+  if (!usernameValid) {
+    username.setCustomValidity("This username is already taken.");
   }
 }
 
-const debCheckUsernameAvailable = debounce(checkUsernameAvailable, 1000);
+const debCheckUsernameAvailable = debounce(checkUsernameAvailable, 500);
 
 username.addEventListener("input", debCheckUsernameAvailable);
+
+username.addEventListener("input", () => {
+  if (username.validity.tooShort) {
+    username.setCustomValidity("The username needs to be at least 8 characters long.");
+  } if (username.validity.tooLong) {
+    username.setCustomValidity("The username can not exceed 20 characters.");
+  } else {
+    username.setCustomValidity("");
+  }
+});
+
+// Password re-entry field congruency check
+const pw1 = document.getElementById("password");
+const pw2 = document.getElementById("pwConfirm");
+
+function checkCongruency(...fields) {
+  let check;
+  fields.forEach(element => {
+    if (element.value === fields[0].value) {
+      check = true;
+    } else {
+      check = false;
+    }
+  });
+  console.log(check);
+}
+
+const debCheckCongruency = debounce(checkCongruency, 500);
+
+pw1.addEventListener("input", () => {
+  debCheckCongruency(pw1, pw2);
+});
+pw2.addEventListener("input", () => {
+  debCheckCongruency(pw1, pw2);
+});
